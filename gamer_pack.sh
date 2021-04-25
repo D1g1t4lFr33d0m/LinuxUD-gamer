a94fabd11889cd0edab5f2ac867e5f0607669831
Kacper Bąk <56700396+53jk1@users.noreply.github.com>
2021-04-25 22:09:43 +0200

    Update gamer_pack.sh

--- gamer_pack.sh
+++ gamer_pack.sh
@@ -3,8 +3,8 @@
 sudo dpkg --add-architecture i386
 sudo add-apt-repository ppa:oibaf/graphics-drivers
 sudo add-apt-repository ppa:lutris-team/lutris
-sudo apt update
-sudo apt install inxi
+sudo apt -y update
+sudo apt -y install inxi
 
 
 kartaGraficzna=`inxi -G | grep -c 'Radeon'`
@@ -12,13 +12,13 @@ kartaGraficzna=`inxi -G | grep -c 'Radeon'`
 if [ $kartaGraficzna -lt 1 ]
 then
  echo "Nvidia"
- sudo apt install nvidia-driver-435
- sudo apt install libgl1-mesa-dri:i386 libgl1-mesa-glx:i386
+ sudo apt -y install nvidia-driver-435
+ sudo apt -y install libgl1-mesa-dri:i386 libgl1-mesa-glx:i386
 else
  echo "Radeon"
- sudo apt install libvulkan1 vulkan-tools vulkan-utils
- sudo apt install mesa-utils
+ sudo apt -y install libvulkan1 vulkan-tools vulkan-utils
+ sudo apt -y install mesa-utils
 fi
-sudo apt install wine64
-sudo apt-get install lutris
-sudo apt install steam
+sudo apt -y install wine64
+sudo apt-get -y install lutris
+sudo apt -y install steam
